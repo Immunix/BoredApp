393f56b0949e7b6916078912684c0ae9714603c6
Immunix <mirceagighileanu@yahoo.com>
2022-02-28 23:04:39 +0100

    app done in its simplest form

--- app/src/main/java/com/immunix/boredapp/ui/MainFragment.kt
+++ app/src/main/java/com/immunix/boredapp/ui/MainFragment.kt
@@ -1,9 +1,56 @@
 package com.immunix.boredapp.ui
 
+import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.immunix.boredapp.R
+import com.immunix.boredapp.databinding.FragmentMainBinding
+import com.immunix.boredapp.state.ViewState
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainFragment : Fragment(R.layout.fragment_main) {
+
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: MainViewModel by activityViewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentMainBinding.bind(view)
+
+        binding.refreshData.setOnClickListener {
+            viewModel.fetchData()
+        }
+        observeData()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun observeData() = binding.apply {
+        viewModel.boredActivity.observe(viewLifecycleOwner) { state ->
+            when (state) {
+                is ViewState.Success -> {
+                    activityName.text = state.data.activity
+                    activityType.text = state.data.type
+                    progressBar.visibility = View.GONE
+                    Log.d("OBSERVE", state.data.activity)
+                }
+                is ViewState.Failure -> {
+                    activityName.text = state.msg
+                    progressBar.visibility = View.GONE
+                    Log.d("OBSERVE", state.msg)
+                }
+                ViewState.Loading -> {
+                    progressBar.visibility = View.VISIBLE
+                    Log.d("OBSERVE", "LOADING")
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/immunix/boredapp/ui/MainViewModel.kt
+++ app/src/main/java/com/immunix/boredapp/ui/MainViewModel.kt
@@ -1,13 +1,17 @@
 package com.immunix.boredapp.ui
 
-import androidx.constraintlayout.motion.utils.ViewState
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.immunix.boredapp.repo.MainRepo
+import com.immunix.boredapp.state.ViewState
+import com.immunix.boredapp.util.HasInternetAccess
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.IOException
 import javax.inject.Inject
 
 @HiltViewModel
@@ -18,7 +22,25 @@ class MainViewModel @Inject constructor(
     private val _boredActivity: MutableLiveData<ViewState> = MutableLiveData()
     val boredActivity: LiveData<ViewState> get() = _boredActivity
 
-    fun fetchData() = viewModelScope.launch {
+    init {
+        fetchData()
+    }
 
+    fun fetchData() = viewModelScope.launch {
+        val hasInternet = withContext(Dispatchers.IO) {
+            HasInternetAccess.execute()
+        }
+
+        if (hasInternet) {
+            _boredActivity.value = ViewState.Loading
+            try {
+                val response = repo.getBoredActivity()
+                _boredActivity.value = ViewState.Success(response)
+            } catch (e: IOException) {
+                _boredActivity.value = ViewState.Failure("Couldn't fetch data. ${e.message}")
+            }
+        } else {
+            _boredActivity.value = ViewState.Failure("No internet connection.")
+        }
     }
 }
